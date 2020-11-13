6c8ca06dd6dfd4027dc057c179d48e87cad21322
Ketan <ketantoo@gmail.com>
2020-11-14 02:06:24 +0530

    setting random number to textview

--- DiceApp/app/src/main/java/com/example/diceapp0/MainActivity.kt
+++ DiceApp/app/src/main/java/com/example/diceapp0/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.diceapp0
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollBtn: Button = findViewById(R.id.rollBtn)
+        rollBtn.setOnClickListener {
+            rollDice()
+        }
+
     }
+
+    private fun rollDice() {
+        val resultText = Random().nextInt(6)+1
+        val resultTv:TextView = findViewById(R.id.resultTV)
+        resultTv.text = resultText.toString()
+    }
+
 }
